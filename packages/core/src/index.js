280ed8f7cdeaad23ed1cf5e20ea593d79cbb194c
Mitch Allen <github@mitchallen.com>
2024-02-18 21:12:27 -0500

    feat: removed stray files left over from bad build

--- packages/core/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
-function log(message) {
-    var now = new Date();
-    var timestamp = now.toISOString();
-    console.log("[".concat(timestamp, "] ").concat(message));
-}
-exports.log = log;
